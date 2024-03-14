8a445541c51be9aa3add1bf06ef67e10c1eca595
Tomas Mizera <tomas.mizera@lutraconsulting.co.uk>
2024-03-14 12:46:26 +0100

    trigger autotests

--- Mergin/utils.py
+++ Mergin/utils.py
@@ -14,6 +14,7 @@ import json
 import glob
 import re
 
+
 from qgis.PyQt.QtCore import QSettings, QVariant
 from qgis.PyQt.QtWidgets import QMessageBox, QFileDialog
 from qgis.PyQt.QtGui import QPalette, QColor
